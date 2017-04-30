d55346aaf6872e921d61198765fea95a2ed2f36f
Enrique Castañeda <mrstinky64@gmail.com>
2017-04-29 18:54:48 -0600

    renderizado de vistas desde un metodo

--- public/index.php
+++ public/index.php
@@ -6,12 +6,22 @@ error_reporting(E_ALL);
 require_once '../vendor/autoload.php';
 include_once '../config.php';
 
-$baseUrl = '';
-$baseDir = $_SERVER['SCRIPT_NAME'];
+$baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']), '',$_SERVER['SCRIPT_NAME']);
+$baseUrl = 'http://'.$_SERVER['HTTP_HOST'].$baseDir;
+define('BASE_URL', $baseUrl);
 
-var_dump($baseDir);
+
 
 $route = array_key_exists('route', $_GET) ? $_GET['route'] : '/' ;
+
+function render($fileName, $params = []){
+    ob_start();
+    extract($params);
+    include $fileName;
+
+
+    return ob_get_clean();
+}
 
 use Phroute\Phroute\RouteCollector;
 
@@ -22,7 +32,8 @@ $router->get('/', function() use ($pdo){
     $query->execute();
 
     $blogPosts= $query->fetchAll(PDO::FETCH_ASSOC);
-    include '../views/index.php';
+    return render('../views/index.php', ['blogPosts' => $blogPosts]);
+
 });
 
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
